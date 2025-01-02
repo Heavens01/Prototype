21c5c0a4d300fd20f8f53c39950fd89ab19465a6
Heavens01 <157021308+Heavens01@users.noreply.github.com>
2025-01-02 18:42:47 +0100

    Add files via upload

---
+++ admin/includes/languages/english.php
@@ -0,0 +1,25 @@
+<?php
+
+	function lang($phrase) {
+
+		static $lang = array(
+
+			// Navbar Links
+
+			'HOME_ADMIN' 	=> 'Home',
+			'CATEGORIES' 	=> 'Categories',
+			'DESIGNS' 		=> 'Designs',
+			'MEMBERS' 		=> 'Members',
+			'FEEDBACKS'		=> 'Feedbacks',
+			'STATISTICS' 	=> 'Statistics',
+			'LOGS' 			=> 'Logs',
+			'' => '',
+			'' => '',
+			'' => '',
+			'' => '',
+			'' => ''
+		);
+
+		return $lang[$phrase];
+
+	}
